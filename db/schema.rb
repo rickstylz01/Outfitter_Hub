eb9a569e002b5e80840a9699c6d2dc82a81ad0be
Ricardo Maya <rickmaya@gmail.com>
2025-05-08 16:30:18 -0500

    edit views and remove decimal attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_08_181445) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_08_212243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,7 +31,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_08_181445) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "price"
-    t.string "decimal"
     t.bigint "brand_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
