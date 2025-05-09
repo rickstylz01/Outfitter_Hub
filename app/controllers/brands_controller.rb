0182919ec635a2109f640b691b1e6fe35b7047cb
Ricardo Maya <rickmaya@gmail.com>
2025-05-09 18:08:06 -0500

    chore: configure RuboCop to exclude ERB files

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,5 +1,5 @@
 class BrandsController < ApplicationController
-  before_action :set_brand, only: %i[ show edit update destroy ]
+  before_action :set_brand, only: %i[show edit update destroy]
 
   # GET /brands or /brands.json
   def index
@@ -58,13 +58,14 @@ class BrandsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_brand
-      @brand = Brand.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def brand_params
-      params.require(:brand).permit(:name, :description)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_brand
+    @brand = Brand.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def brand_params
+    params.require(:brand).permit(:name, :description)
+  end
 end
